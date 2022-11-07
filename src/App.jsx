155bd1db3743dc1332652b6511cf958621d74d4f
ostinweb <web.ostapuk@gmail.com>
2022-11-07 20:50:07 +0300

    add: delete comments

--- src/App.jsx
+++ src/App.jsx
@@ -1,39 +1,64 @@
 import { useState } from 'react'
-import Todo from './components/Todo'
 import { generateId } from './utils/generateId'
+
+//components
 import Count from './components/Count'
 import Input from './components/Input'
 import Layout from './Layout/Layout'
 import Header from './components/Header'
+import Todo from './components/Todo'
+
 export default function App() {
   const [state, setState] = useState('')
   const [todos, setTodos] = useState([])
+  //сетим состояние Input text
   const handleSetState = (e) => {
     setState(e.target.value)
   }
+  //добавляем объект Todo в todos
   const handleAddTodos = () => {
+    // проверка на пустую todo
+    if (state.trim() === '') {
+      alert('oops')
+      return
+    }
     setTodos(todos.concat({ text: state, done: false, id: generateId() }))
     setState('')
   }
+  //сетим текущий done в todos
+  //проверяем по id, а не по name => профит
   const handleSetDone = (id) => {
     const todosCopy = [...todos]
     const current = todosCopy.find((item) => item.id === id)
     current.done = !current.done
     setTodos(todosCopy)
   }
+  //удалием todo
+  const handleDeleteTodo = (id) => {
+    const todosDelete = [...todos]
+    const deleteIndex = todosDelete.findIndex((item) => item.id === id)
+    todosDelete.splice(deleteIndex, 1)
+    setTodos(todosDelete)
+  }
+
   return (
     <>
       <Header />
       <Layout>
         <Input
-          handleAddTodos={handleAddTodos}
           state={state}
+          handleAddTodos={handleAddTodos}
           handleSetState={handleSetState}
         />
         <Count todos={todos} />
       </Layout>
       {todos.map((todo) => (
-        <Todo handleSetDone={handleSetDone} todo={todo} key={todo.id} />
+        <Todo
+          handleSetDone={handleSetDone}
+          todo={todo}
+          key={todo.id}
+          handleDeleteTodo={handleDeleteTodo}
+        />
       ))}
     </>
   )
--- src/components/Todo.jsx
+++ src/components/Todo.jsx
@@ -1,4 +1,4 @@
-export default function Todo({ todo, handleSetDone }) {
+export default function Todo({ todo, handleSetDone, handleDeleteTodo }) {
   return (
     <div className="flex justify-center">
       <div className="w-1/2  m-3 bg-slate-300">
@@ -10,11 +10,13 @@ export default function Todo({ todo, handleSetDone }) {
               checked={todo.done}
             />
           </div>
+          {/*проверка done -> меняеем стили у todo*/}
           <div className={todo.done ? 'text-slate-400' : ''}>
             <p>{todo.text}</p>
           </div>
         </div>
       </div>
+      <button onClick={() => handleDeleteTodo(todo.id)}>Удалить</button>
     </div>
   )
 }
